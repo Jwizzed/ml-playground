09ebb97a0be0866fa759836a7512428df4d68e61
Krittin SETDHAVANICH <krittin.set@ku.th>
2024-02-07 21:04:18 +0700

    Add a progress bar

--- ml_section/ml.py
+++ ml_section/ml.py
@@ -29,17 +29,26 @@ class MachineLearningApp:
                                      self.df.columns, key='class_target')
 
         if st.button('Train Classification Model', key='class_train'):
+            progress_bar = st.progress(0)
             st.write("Running setup for classification... Please wait.")
+            progress_bar.progress(10)
+
             setup_class(data=self.df, target=chosen_target, html=False,
                         verbose=False)
+            progress_bar.progress(50)
             st.write("Setup complete.")
 
             st.markdown("### Classification Experiment Settings")
             setup_df = pull_class()
             st.dataframe(setup_df.iloc[:, :2])
 
-            st.write("Comparing classification models... Please wait.")
+            st.write("Comparing classification models... Please wait. (Might take a few minutes)")
+            progress_bar.progress(
+                75)
+
             best_model = compare_models_class()
+            progress_bar.progress(
+                100)
             st.write("Model comparison complete.")
 
             st.markdown("### Best Classification Model")
@@ -48,6 +57,7 @@ class MachineLearningApp:
 
             st.write("Best Model:")
             st.write(best_model)
+            progress_bar.empty()
 
     def run_regression(self):
         """Run the regression section of the application."""
@@ -55,17 +65,24 @@ class MachineLearningApp:
                                      self.df.columns, key='reg_target')
 
         if st.button('Train Regression Model', key='reg_train'):
+            progress_bar = st.progress(0)
             st.write("Running setup for regression... Please wait.")
+            progress_bar.progress(10)
+
             setup_reg(data=self.df, target=chosen_target, html=False,
                       verbose=False)
+            progress_bar.progress(50)
             st.write("Setup complete.")
 
             st.markdown("### Regression Experiment Settings")
             setup_df = pull_reg()
             st.dataframe(setup_df.iloc[:, :2])
 
-            st.write("Comparing regression models... Please wait.")
+            st.write("Comparing regression models... Please wait. (Might take a few minutes)")
+            progress_bar.progress(75)
+
             best_model = compare_models_reg()
+            progress_bar.progress(100)
             st.write("Model comparison complete.")
 
             st.markdown("### Best Regression Model")
@@ -74,3 +91,4 @@ class MachineLearningApp:
 
             st.write("Best Model:")
             st.write(best_model)
+            progress_bar.empty()
