576a9595369c306b5bf2f7a8a6d9649940d8f1a5
Krittin SETDHAVANICH <krittin.set@ku.th>
2024-02-07 14:07:54 +0700

    refactor lv2

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 from eda_section.eda import EDAApp
 
 
-def main():
+def info():
+    """Display the information about the application."""
     st.sidebar.title("ML Playground")
     st.sidebar.header("Navigation")
     options = ["Exploratory Data Analysis", "Machine Learning",
@@ -12,14 +13,20 @@ def main():
 
     st.sidebar.header("About")
     st.sidebar.info("""
-        ML Playground is an interactive tool designed to simplify and enhance data management tasks. It provides users with the ability to:
+            ML Playground is an interactive tool designed to simplify and enhance data management tasks. It provides users with the ability to:
 
-        - Perform Exploratory Data Analysis (EDA)
-        - Establish Machine Learning Baselines
-        - And much more!
+            - Perform Exploratory Data Analysis (EDA)
+            - Establish Machine Learning Baselines
+            - And much more!
 
-        Dive into the world of data with ease and discover insights through an intuitive interface.
-    """)
+            Dive into the world of data with ease and discover insights through an intuitive interface.
+        """)
+    return choice
+
+
+def main():
+    """Main function to run the application."""
+    choice = info()
 
     if choice == "Exploratory Data Analysis":
         EDAApp().run()
--- eda_section/statistics.py
+++ eda_section/statistics.py
@@ -7,27 +7,43 @@ class SummaryStatistics:
     def __init__(self, df, tab):
         self.df = df
         self.tab = tab
+        self.col1, self.col2, self.col3 = st.columns(3)
 
     def display_summary_statistics(self):
         """Display the summary statistics of the dataframe."""
         with self.tab:
-            col1, col2, col3 = st.columns(3)
-            with col1:
-                st.write("Missing values:",
-                         self.df.isnull().sum().rename("Number of null"))
-            with col2:
-                st.write("Data types:", self.df.dtypes.rename("Dtype"))
-            with col3:
-                st.write("Describe:", self.df.describe())
+            self.display_missing_values()
+            self.display_data_types()
+            self.display_descriptive_statistics()
+            self.display_duplicated_rows()
+            self.display_constant_columns()
 
-            if self.df.duplicated().sum() > 0:
-                st.write("There are duplicated rows in the dataset.")
-            else:
-                st.write("No duplicated rows in the dataset.")
+    def display_missing_values(self):
+        """Display the number of missing values."""
+        with self.col1:
+            st.write("Missing values:", self.df.isnull().sum().rename("Number of null"))
 
-            constant_columns = [col for col in self.df.columns if
-                                self.df[col].nunique() == 1]
-            if constant_columns:
-                st.write(f"Constant columns: {constant_columns}")
-            else:
-                st.write("No constant columns in the dataset.")
+    def display_data_types(self):
+        """Display the data types of columns."""
+        with self.col2:
+            st.write("Data types:", self.df.dtypes.rename("Dtype"))
+
+    def display_descriptive_statistics(self):
+        """Display descriptive statistics of numerical columns."""
+        with self.col3:
+            st.write("Describe:", self.df.describe())
+
+    def display_duplicated_rows(self):
+        """Display whether there are duplicated rows."""
+        if self.df.duplicated().sum() > 0:
+            st.write("There are duplicated rows in the dataset.")
+        else:
+            st.write("No duplicated rows in the dataset.")
+
+    def display_constant_columns(self):
+        """Display constant columns in the dataset."""
+        constant_columns = [col for col in self.df.columns if self.df[col].nunique() == 1]
+        if constant_columns:
+            st.write(f"Constant columns: {constant_columns}")
+        else:
+            st.write("No constant columns in the dataset.")
